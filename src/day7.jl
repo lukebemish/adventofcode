f56cc5c93437661723cf4eafa0243e8e57d24aad
Luke Bemish <lukebemish@lukebemish.dev>
2024-12-06 23:19:35 -0600

    Add code to time

--- src/day7.jl
+++ src/day7.jl
@@ -1,68 +1,72 @@
-input = map(readlines("data/day7.txt")) do line
-    parts = split(line, " ")
-    parts[1] = parts[1][1:end-1]
-    map(parts) do part parse(Int, part) end
-end
-
-## Part 1
-
-total = 0
-for line ∈ input
-    target = line[1]
-    rest = line[2:end]
-    for i ∈ 0:2^(length(rest)-1)-1
-        calculated = rest[1]
-        x = i
-        for j ∈ 2:length(rest)
-            if (x & 1) == 1
-                calculated += rest[j]
-            else
-                calculated *= rest[j]
-            end
-            if calculated > target
-                break
-            end
-            x >>= 1
-        end
-        if calculated == target
-            total += target
-            break
-        end
+function day7()
+    input = map(readlines("data/day7.txt")) do line
+        parts = split(line, " ")
+        parts[1] = parts[1][1:end-1]
+        map(parts) do part parse(Int, part) end
     end
-end
 
-println("Part 1: $total")
-
-## Part 2
-
-total = 0
-for line ∈ input
-    target = line[1]
-    rest = line[2:end]
-    for i ∈ 0:3^(length(rest)-1)-1
-        calculated = rest[1]
-        x = i
-        for j ∈ 2:length(rest)
-            if (x % 3) == 1
-                calculated += rest[j]
-            elseif (x % 3) == 2
-                calculated *= rest[j]
-            else
-                calculated *= 10^length(string(rest[j]))
-                calculated += rest[j]
+    function part1()
+        total = 0
+        for line ∈ input
+            target = line[1]
+            rest = line[2:end]
+            for i ∈ 0:2^(length(rest)-1)-1
+                calculated = rest[1]
+                x = i
+                for j ∈ 2:length(rest)
+                    if (x & 1) == 1
+                        calculated += rest[j]
+                    else
+                        calculated *= rest[j]
+                    end
+                    if calculated > target
+                        break
+                    end
+                    x >>= 1
+                end
+                if calculated == target
+                    total += target
+                    break
+                end
             end
-            if calculated > target
-                break
-            end
-            x = div(x, 3)
         end
-        if calculated == target
-            total += target
-            break
-        end
+
+        println("Part 1: $total")
     end
+
+    @time part1()
+
+    function part2()
+        total = 0
+        for line ∈ input
+            target = line[1]
+            rest = line[2:end]
+            for i ∈ 0:3^(length(rest)-1)-1
+                calculated = rest[1]
+                x = i
+                for j ∈ 2:length(rest)
+                    if (x % 3) == 1
+                        calculated += rest[j]
+                    elseif (x % 3) == 2
+                        calculated *= rest[j]
+                    else
+                        calculated *= 10^length(string(rest[j]))
+                        calculated += rest[j]
+                    end
+                    if calculated > target
+                        break
+                    end
+                    x = div(x, 3)
+                end
+                if calculated == target
+                    total += target
+                    break
+                end
+            end
+        end
+
+        println("Part 2: $total")
+    end
+
+    @time part2()
 end
-
-println("Part 2: $total")
-
-##
