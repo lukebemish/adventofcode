0144d84aa223897859ae6e5b4ea17f04d076c3ed
Luke Bemish <lukebemish@lukebemish.dev>
2024-12-23 01:16:56 -0600

    Improve day 22 part 2

--- src/day22.jl
+++ src/day22.jl
@@ -32,42 +32,18 @@ day22() = begin
         end
         changes = prices[2:end, :] .- prices[1:end-1, :]
 
-        @views bananas(sequence) = begin
-            foridx(i) = begin
-                for j ∈ 1:(2000-3)
-                    fits = true
-                    for k ∈ 0:(length(sequence)-1)
-                        if changes[j+k, i] != sequence[k+1]
-                            fits = false
-                            break
-                        end
-                    end
-                    if fits
-                        return prices[j+(length(sequence)), i]
-                    end
+        totals = Dict()
+        for i ∈ 1:length(originalsecrets)
+            found = Dict()
+            for j ∈ 1:(2000-3)
+                sequence = changes[j:j+3, i]
+                if !haskey(found, sequence)
+                    found[sequence] = nothing
+                    totals[sequence] = get(totals, sequence, 0) + prices[j+4, i]
                 end
-                return 0
-            end
-            total = 0
-            for i ∈ 1:length(originalsecrets)
-                total += foridx(i)
-            end
-            total
-        end
-
-        sequences = Set{Vector{Int}}()
-        for j ∈ 1:(2000-3)
-            for i ∈ 1:length(originalsecrets)
-                push!(sequences, changes[j:j+3, i])
             end
         end
-        sequences = collect(sequences)
 
-        foundbananas = zeros(Int, length(sequences))
-        Threads.@threads for i ∈ eachindex(sequences)
-            foundbananas[i] = bananas(sequences[i])
-        end
-
-        println("Part 2: $(maximum(foundbananas))")
+        println("Part 2: $(maximum(values(totals)))")
     end
 end
